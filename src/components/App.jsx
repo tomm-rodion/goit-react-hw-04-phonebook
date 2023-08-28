c21c2cfb193fe5b8dab32e76fac77463113b4aa5
tomm-rodion <autovolna@gmail.com>
2023-08-28 18:47:23 +0300

    corection dode

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,55 +14,42 @@ const getInitialContacts = () => {
   if (savedUpContacts !== null) {
     const parsContacts = JSON.parse(savedUpContacts);
     return parsContacts;
-  } else {
-    return [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ];
   }
+  return [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
 };
 
 export const App = () => {
   const [contacts, setContacts] = useState(getInitialContacts);
-
   const [filter, setFilter] = useState('');
-
-  const onformSubmit = ({ id, name, number }) => {
-    setContacts(prevContacts => [{ id, name, number }, ...prevContacts]);
-  };
-
-  const onDelete = id => {
-    const resultSortedContacts = contacts.filter(contact => contact.id !== id);
-    console.log(resultSortedContacts);
-    setContacts(prevContacts => {
-      return [...resultSortedContacts];
-    });
-  };
-
-  const onFilter = e => {
-    console.log(e.currentTarget.value);
-    setFilter(e.currentTarget.value);
-  };
-
-  const onFilterContacts = () => {
-    let filterContact = [];
-    if (filter) {
-      filterContact = contacts.filter(
-        contact =>
-          contact.name.includes(filter) ||
-          contact.name.toLowerCase().includes(filter)
-      );
-    } else {
-      return contacts;
-    }
-    return filterContact;
-  };
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
+
+  const onformSubmit = newContact => {
+    setContacts(prevContacts => [newContact, ...prevContacts]);
+  };
+
+  const onDelete = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
+  };
+
+  const onFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const onFilterContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
 
   return (
     <Wrapper>
